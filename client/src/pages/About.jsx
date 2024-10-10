bcb1d18cc077269f6072a920e8236f8e4e65c90a
Benjamin Brechter <benbrechter3@gmail.com>
2024-10-10 18:59:30 -0500

    just giivee me the green

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,9 @@
+import HomeBtn from "./componets/home-btn"
 
 function About() {
     return(
         <div>
+            <div><HomeBtn/></div>
             <h1>What this is all about!</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque similique nemo illo dicta odit est, eaque beatae dolorum sapiente veritatis quibusdam eius expedita culpa provident dolores doloremque officia sed maxime?</p>
         </div>
