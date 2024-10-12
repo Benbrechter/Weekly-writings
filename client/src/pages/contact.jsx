998d979d63d207953f28e4c3d7435bb36eb6c40e
unknown <brechterben8@gmail.com>
2024-10-12 11:25:17 -0500

    Contact layout ok

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -4,7 +4,20 @@ function Contact(){
     return(
         <div>
             <div><HomeBtn/></div>
-            <h1>Meow</h1>
+            <h1>Suggestions inbox</h1>
+            <p>Please leave any suggestions you would like me to hear. Improvements for the website or Story ideas.</p>
+            <div>
+             <form action="submit">
+                <h3>Alias</h3>
+                <input type="text" placeholder="enter your alias"/>
+                <h3>Reasoning</h3>
+                <input type="text" placeholder="Why?" />
+                <h3 id="message">Message</h3>
+                <textarea name="message" type = "text" placeholder="Go easy on me"></textarea>
+                <button>MEOW!!</button>
+            </form>   
+            </div>
+            
         </div>
     )
 }
